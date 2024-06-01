ac9008080ed559dc4dbe87bd9e7325f2547f8956
Nazril <inazril455@gmail.com>
2024-06-01 14:53:14 +0700

    Penambahan Kotak

--- notfiy/Views/Other/PopUpNotif.Designer.cs
+++ notfiy/Views/Other/PopUpNotif.Designer.cs
@@ -28,10 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PopUpNotif));
+            kryptonLabel1 = new Krypton.Toolkit.KryptonLabel();
+            kryptonPictureBox1 = new Krypton.Toolkit.KryptonPictureBox();
+            kryptonPictureBox2 = new Krypton.Toolkit.KryptonPictureBox();
+            ((System.ComponentModel.ISupportInitialize)kryptonPictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)kryptonPictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // kryptonLabel1
+            // 
+            kryptonLabel1.Location = new Point(0, 14);
+            kryptonLabel1.Name = "kryptonLabel1";
+            kryptonLabel1.Size = new Size(284, 48);
+            kryptonLabel1.StateNormal.ShortText.Color1 = Color.Black;
+            kryptonLabel1.StateNormal.ShortText.Color2 = Color.Black;
+            kryptonLabel1.StateNormal.ShortText.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            kryptonLabel1.TabIndex = 0;
+            kryptonLabel1.Values.Text = "User Rafky telah membagikan\r\nvitamin buat ibu hamil";
+            // 
+            // kryptonPictureBox1
+            // 
+            kryptonPictureBox1.Image = (Image)resources.GetObject("kryptonPictureBox1.Image");
+            kryptonPictureBox1.Location = new Point(306, 15);
+            kryptonPictureBox1.Name = "kryptonPictureBox1";
+            kryptonPictureBox1.Size = new Size(48, 48);
+            kryptonPictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            kryptonPictureBox1.TabIndex = 1;
+            kryptonPictureBox1.TabStop = false;
+            // 
+            // kryptonPictureBox2
+            // 
+            kryptonPictureBox2.Image = (Image)resources.GetObject("kryptonPictureBox2.Image");
+            kryptonPictureBox2.Location = new Point(360, 15);
+            kryptonPictureBox2.Name = "kryptonPictureBox2";
+            kryptonPictureBox2.Size = new Size(57, 47);
+            kryptonPictureBox2.SizeMode = PictureBoxSizeMode.AutoSize;
+            kryptonPictureBox2.TabIndex = 2;
+            kryptonPictureBox2.TabStop = false;
+            // 
+            // PopUpNotif
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(kryptonPictureBox2);
+            Controls.Add(kryptonPictureBox1);
+            Controls.Add(kryptonLabel1);
+            Name = "PopUpNotif";
+            Size = new Size(431, 89);
+            ((System.ComponentModel.ISupportInitialize)kryptonPictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)kryptonPictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Krypton.Toolkit.KryptonLabel kryptonLabel1;
+        private Krypton.Toolkit.KryptonPictureBox kryptonPictureBox1;
+        private Krypton.Toolkit.KryptonPictureBox kryptonPictureBox2;
     }
 }
