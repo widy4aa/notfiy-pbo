7489f0bba8933306d4df2a42070f8d214f96d97d
Nazril <inazril455@gmail.com>
2024-06-10 16:48:02 +0700

    menambahkan detail note

--- notfiy/Views/Homepage/HomepageDetail.Designer.cs
+++ notfiy/Views/Homepage/HomepageDetail.Designer.cs
@@ -37,9 +37,9 @@
             kryptonListBox1 = new Krypton.Toolkit.KryptonListBox();
             kryptonBorderEdge1 = new Krypton.Toolkit.KryptonBorderEdge();
             NoteName = new Krypton.Toolkit.KryptonLabel();
-            BtnBack = new Krypton.Toolkit.KryptonButton();
-            BtnEdit = new Krypton.Toolkit.KryptonButton();
-            BtnDelete = new Krypton.Toolkit.KryptonButton();
+            ButtonBack = new Krypton.Toolkit.KryptonButton();
+            ButtonEdit = new Krypton.Toolkit.KryptonButton();
+            ButtonTrash = new Krypton.Toolkit.KryptonButton();
             flowLayoutPanel1 = new FlowLayoutPanel();
             NotePictureBox = new Krypton.Toolkit.KryptonPictureBox();
             NoteContentTextBox = new Krypton.Toolkit.KryptonRichTextBox();
@@ -209,7 +209,7 @@
             // 
             kryptonListBox1.Location = new Point(380, 212);
             kryptonListBox1.Name = "kryptonListBox1";
-            kryptonListBox1.Size = new Size(861, 605);
+            kryptonListBox1.Size = new Size(861, 671);
             kryptonListBox1.StateCommon.Border.Color1 = Color.Black;
             kryptonListBox1.StateCommon.Border.Color2 = Color.Black;
             kryptonListBox1.StateCommon.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.Top | Krypton.Toolkit.PaletteDrawBorders.Bottom | Krypton.Toolkit.PaletteDrawBorders.Left | Krypton.Toolkit.PaletteDrawBorders.Right;
@@ -219,7 +219,7 @@
             // 
             // kryptonBorderEdge1
             // 
-            kryptonBorderEdge1.Location = new Point(380, 729);
+            kryptonBorderEdge1.Location = new Point(380, 809);
             kryptonBorderEdge1.Name = "kryptonBorderEdge1";
             kryptonBorderEdge1.Size = new Size(861, 3);
             kryptonBorderEdge1.StateCommon.Color1 = Color.Black;
@@ -239,58 +239,62 @@
             NoteName.Values.Text = "Judul Note";
             NoteName.Click += kryptonLabel2_Click;
             // 
-            // BtnBack
+            // ButtonBack
             // 
-            BtnBack.Location = new Point(1094, 757);
-            BtnBack.Name = "BtnBack";
-            BtnBack.Size = new Size(110, 31);
-            BtnBack.StateCommon.Back.Color1 = Color.FromArgb(165, 165, 165);
-            BtnBack.StateCommon.Back.Color2 = Color.FromArgb(165, 165, 165);
-            BtnBack.StateCommon.Border.Color1 = Color.Black;
-            BtnBack.StateCommon.Border.Color2 = Color.Black;
-            BtnBack.StateCommon.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.Top | Krypton.Toolkit.PaletteDrawBorders.Bottom | Krypton.Toolkit.PaletteDrawBorders.Left | Krypton.Toolkit.PaletteDrawBorders.Right;
-            BtnBack.StateCommon.Border.Rounding = 20F;
-            BtnBack.StateCommon.Border.Width = 1;
-            BtnBack.StateNormal.Content.ShortText.Color1 = Color.Black;
-            BtnBack.StateNormal.Content.ShortText.Color2 = Color.Black;
-            BtnBack.StateNormal.Content.ShortText.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            BtnBack.TabIndex = 26;
-            BtnBack.Values.Text = "Back";
+            ButtonBack.Location = new Point(1094, 829);
+            ButtonBack.Name = "ButtonBack";
+            ButtonBack.Size = new Size(110, 31);
+            ButtonBack.StateCommon.Back.Color1 = Color.FromArgb(165, 165, 165);
+            ButtonBack.StateCommon.Back.Color2 = Color.FromArgb(165, 165, 165);
+            ButtonBack.StateCommon.Border.Color1 = Color.Black;
+            ButtonBack.StateCommon.Border.Color2 = Color.Black;
+            ButtonBack.StateCommon.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.Top | Krypton.Toolkit.PaletteDrawBorders.Bottom | Krypton.Toolkit.PaletteDrawBorders.Left | Krypton.Toolkit.PaletteDrawBorders.Right;
+            ButtonBack.StateCommon.Border.Rounding = 20F;
+            ButtonBack.StateCommon.Border.Width = 1;
+            ButtonBack.StateNormal.Content.ShortText.Color1 = Color.Black;
+            ButtonBack.StateNormal.Content.ShortText.Color2 = Color.Black;
+            ButtonBack.StateNormal.Content.ShortText.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            ButtonBack.TabIndex = 26;
+            ButtonBack.Values.Text = "Back";
+            ButtonBack.Click += ButtonBack_Click;
             // 
-            // BtnEdit
+            // ButtonEdit
             // 
-            BtnEdit.Location = new Point(946, 757);
-            BtnEdit.Name = "BtnEdit";
-            BtnEdit.Size = new Size(120, 31);
-            BtnEdit.StateCommon.Back.Color1 = Color.Yellow;
-            BtnEdit.StateCommon.Back.Color2 = Color.Yellow;
-            BtnEdit.StateCommon.Border.Color1 = Color.Black;
-            BtnEdit.StateCommon.Border.Color2 = Color.Black;
-            BtnEdit.StateCommon.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.Top | Krypton.Toolkit.PaletteDrawBorders.Bottom | Krypton.Toolkit.PaletteDrawBorders.Left | Krypton.Toolkit.PaletteDrawBorders.Right;
-            BtnEdit.StateCommon.Border.Rounding = 20F;
-            BtnEdit.StateCommon.Border.Width = 1;
-            BtnEdit.StateNormal.Content.ShortText.Color1 = Color.Black;
-            BtnEdit.StateNormal.Content.ShortText.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            BtnEdit.TabIndex = 27;
-            BtnEdit.Values.Text = "Edit";
+            ButtonEdit.Location = new Point(946, 829);
+            ButtonEdit.Name = "ButtonEdit";
+            ButtonEdit.Size = new Size(120, 31);
+            ButtonEdit.StateCommon.Back.Color1 = Color.Yellow;
+            ButtonEdit.StateCommon.Back.Color2 = Color.Yellow;
+            ButtonEdit.StateCommon.Border.Color1 = Color.Black;
+            ButtonEdit.StateCommon.Border.Color2 = Color.Black;
+            ButtonEdit.StateCommon.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.Top | Krypton.Toolkit.PaletteDrawBorders.Bottom | Krypton.Toolkit.PaletteDrawBorders.Left | Krypton.Toolkit.PaletteDrawBorders.Right;
+            ButtonEdit.StateCommon.Border.Rounding = 20F;
+            ButtonEdit.StateCommon.Border.Width = 1;
+            ButtonEdit.StateNormal.Content.ShortText.Color1 = Color.Black;
+            ButtonEdit.StateNormal.Content.ShortText.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            ButtonEdit.TabIndex = 27;
+            ButtonEdit.Values.Text = "Edit";
+            ButtonEdit.Click += ButtonEdit_Click;
             // 
-            // BtnDelete
+            // ButtonTrash
             // 
-            BtnDelete.Location = new Point(421, 757);
-            BtnDelete.Name = "BtnDelete";
-            BtnDelete.Size = new Size(130, 31);
-            BtnDelete.StateCommon.Back.Color1 = Color.FromArgb(255, 107, 107);
-            BtnDelete.StateCommon.Back.Color2 = Color.FromArgb(255, 107, 107);
-            BtnDelete.StateCommon.Border.Color1 = Color.Black;
-            BtnDelete.StateCommon.Border.Color2 = Color.Black;
-            BtnDelete.StateCommon.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.Top | Krypton.Toolkit.PaletteDrawBorders.Bottom | Krypton.Toolkit.PaletteDrawBorders.Left | Krypton.Toolkit.PaletteDrawBorders.Right;
-            BtnDelete.StateCommon.Border.Rounding = 20F;
-            BtnDelete.StateCommon.Border.Width = 1;
-            BtnDelete.StateNormal.Content.ShortText.Color1 = Color.Black;
-            BtnDelete.StateNormal.Content.ShortText.Color2 = Color.Black;
-            BtnDelete.StateNormal.Content.ShortText.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            BtnDelete.TabIndex = 28;
-            BtnDelete.Values.Text = "Delete";
+            ButtonTrash.Enabled = false;
+            ButtonTrash.Location = new Point(421, 829);
+            ButtonTrash.Name = "ButtonTrash";
+            ButtonTrash.Size = new Size(130, 31);
+            ButtonTrash.StateCommon.Back.Color1 = Color.FromArgb(255, 107, 107);
+            ButtonTrash.StateCommon.Back.Color2 = Color.FromArgb(255, 107, 107);
+            ButtonTrash.StateCommon.Border.Color1 = Color.Black;
+            ButtonTrash.StateCommon.Border.Color2 = Color.Black;
+            ButtonTrash.StateCommon.Border.DrawBorders = Krypton.Toolkit.PaletteDrawBorders.Top | Krypton.Toolkit.PaletteDrawBorders.Bottom | Krypton.Toolkit.PaletteDrawBorders.Left | Krypton.Toolkit.PaletteDrawBorders.Right;
+            ButtonTrash.StateCommon.Border.Rounding = 20F;
+            ButtonTrash.StateCommon.Border.Width = 1;
+            ButtonTrash.StateNormal.Content.ShortText.Color1 = Color.Black;
+            ButtonTrash.StateNormal.Content.ShortText.Color2 = Color.Black;
+            ButtonTrash.StateNormal.Content.ShortText.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            ButtonTrash.TabIndex = 28;
+            ButtonTrash.Values.Text = "Trash";
+            ButtonTrash.Click += ButtonTrash_Click;
             // 
             // flowLayoutPanel1
             // 
@@ -299,24 +303,23 @@
             flowLayoutPanel1.FlowDirection = FlowDirection.TopDown;
             flowLayoutPanel1.Location = new Point(398, 304);
             flowLayoutPanel1.Name = "flowLayoutPanel1";
-            flowLayoutPanel1.Size = new Size(815, 419);
+            flowLayoutPanel1.Size = new Size(815, 499);
             flowLayoutPanel1.TabIndex = 30;
             // 
             // NotePictureBox
             // 
             NotePictureBox.Location = new Point(3, 3);
             NotePictureBox.Name = "NotePictureBox";
-            NotePictureBox.Size = new Size(100, 50);
-            NotePictureBox.SizeMode = PictureBoxSizeMode.AutoSize;
+            NotePictureBox.Size = new Size(803, 125);
             NotePictureBox.TabIndex = 0;
             NotePictureBox.TabStop = false;
             NotePictureBox.Visible = false;
             // 
             // NoteContentTextBox
             // 
-            NoteContentTextBox.Location = new Point(3, 59);
+            NoteContentTextBox.Location = new Point(3, 134);
             NoteContentTextBox.Name = "NoteContentTextBox";
-            NoteContentTextBox.Size = new Size(812, 334);
+            NoteContentTextBox.Size = new Size(812, 354);
             NoteContentTextBox.TabIndex = 1;
             NoteContentTextBox.Text = "kryptonRichTextBox1";
             // 
@@ -337,9 +340,9 @@
             BackColor = Color.White;
             Controls.Add(kryptonLabel3);
             Controls.Add(flowLayoutPanel1);
-            Controls.Add(BtnDelete);
-            Controls.Add(BtnEdit);
-            Controls.Add(BtnBack);
+            Controls.Add(ButtonTrash);
+            Controls.Add(ButtonEdit);
+            Controls.Add(ButtonBack);
             Controls.Add(NoteName);
             Controls.Add(kryptonBorderEdge1);
             Controls.Add(kryptonPanel1);
@@ -352,7 +355,6 @@
             kryptonPanel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)kryptonPictureBox1).EndInit();
             flowLayoutPanel1.ResumeLayout(false);
-            flowLayoutPanel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)NotePictureBox).EndInit();
             ResumeLayout(false);
             PerformLayout();
@@ -367,9 +369,9 @@
         private Krypton.Toolkit.KryptonListBox kryptonListBox1;
         private Krypton.Toolkit.KryptonBorderEdge kryptonBorderEdge1;
         private Krypton.Toolkit.KryptonLabel NoteName;
-        private Krypton.Toolkit.KryptonButton BtnBack;
-        private Krypton.Toolkit.KryptonButton BtnEdit;
-        private Krypton.Toolkit.KryptonButton BtnDelete;
+        private Krypton.Toolkit.KryptonButton ButtonBack;
+        private Krypton.Toolkit.KryptonButton ButtonEdit;
+        private Krypton.Toolkit.KryptonButton ButtonTrash;
         private FlowLayoutPanel flowLayoutPanel1;
         private Krypton.Toolkit.KryptonPictureBox NotePictureBox;
         private Krypton.Toolkit.KryptonRichTextBox NoteContentTextBox;
